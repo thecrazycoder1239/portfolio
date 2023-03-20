34470633ed77999121b07fedbc02f6a8cd90aeff
thecrazycoder1239 <tombarrett28@icloud.com>
2023-03-20 00:46:44 +0000

    adding cover letter questions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 function App() {
   return (
     <>
+    <h1>Thomas Barrett's Portfolio Site</h1>
+    <p>ATTENTION GOOGLE RECRUITER</p><br></br>
+    <p>I am really sorry but I forgot to attach my motivational letter / answers to the short questions you requested for the apprenticeship</p>
+    <br></br> 
+    <p>here are my answers</p>
+    <br></br>
+    <p>
+I don't believe there is a greater opportunity out there for me to learn and grow as a software engineer. I am desperate to work with inspirational Googlers that push me to become the best version of myself. In my opinion, google is the epicenter of innovation. I want to be at the forefront of that innovation and be a part of something big, it's just so exhilarating to think about. <br/><br/>
+
+The rapid rate of development in the software engineering space right now is frightening, but thrilling. With the age of AI and machine learning being well and truly here, I want to be a part of a company that utilises these new technologies to create things that we never thought were this close to being possible. The uncertainty in the future of Software Engineering is absolutely what excites me most. Who knows what will be possible even later this year? Google will definitely be one of the first to know though, and I want to be there to be a part of it.<br/><br/>
+
+Brilliant online resources such as freeCodeCamp, Codecademy and Harvard's CS50x hooked me into the tech space, but the Northcoders coding bootcamp has supercharged my learning. There is also a fantastic community of students / friends who have educated me about their areas of interest (e.g. game development) and keep me up to date on recent developments in the industry (although even then it's hard to keep up).<br/><br/>
+
+I believe what makes me unique is my trait of always standing behind those who need help the most. In football and in school, I would always be the person breaking up fights and scuffles between people, even if it meant that I got ridiculed myself for it. Even if I don't agree with what someone has done, I won't tolerate any bullying of that person. The world is too quick to anger, and I believe there should always be space for forgiveness and gratitude. This was a philosophy I was taught as a young boy, and it has served me well.<br/><br/>
+
+Overall, thank you for considering me for this incredible opportunity. I will be regularly updating my portfolio with new projects (including the final project for CS50 and 13 week coding bootcamp). I am aware of how difficult it is to get into this amazing company, but please know I will absolutely be trying again as soon as I can if I don't get in. It's a dream job worth fighting for :)</p><br/><br/>
     <h1>NC-Games</h1>
     <p>my links for the backend</p>
     <a href="https://github.com/thecrazycoder1239/NC-Games-BE">backend github link</a> <br></br>
