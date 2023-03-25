3c1d1121eea5dde10218c633f396fde6b9a6c476
thecrazycoder1239 <tombarrett28@icloud.com>
2023-03-25 19:40:56 +0000

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Nav () {
+    return (
+        <nav className="nav-container">
+            <li className="nav-item">About</li>
+            <li className="nav-item">Projects</li>
+            <li className="nav-item">Contact Me</li>
+        </nav>
+    )
+}
---
+++ src/components/Links.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Links () {
+    return (
+        <ol className="links">
+            <li>
+                <img src="/github.png" alt="github"/>
+            </li>
+        </ol>
+    )
+}
