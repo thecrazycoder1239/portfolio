14351e9c4baf67f998239c5d48dbc21298ffe588
thecrazycoder1239 <tombarrett28@icloud.com>
2023-03-28 03:06:39 +0100

    responsive for large laptops

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
     <Header/>
     <Links/>
-    <h1 className='title'>Thomas <br></br> Barrett</h1>
+    <h1 className='title'>Thomas Barrett</h1>
     <p className='bio'>Hello there! I am a junior full stack software developer. Thankyou for visiting my portfolio page, I hope you enjoy viewing my projects!</p>
     <div className='purple'></div>
     <div className='blue'></div>
